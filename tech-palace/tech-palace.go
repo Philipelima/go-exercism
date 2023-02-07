979d4b9e5d43ce58c1c0a4b9de27f9e288c5485e
Philipe  Lima <69319899+Philipelima@users.noreply.github.com>
2023-02-06 23:18:44 -0300

    update: tech-palace.go

--- tech-palace/tech-palace.go
+++ tech-palace/tech-palace.go
@@ -4,19 +4,19 @@ import "strings"
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	customerName := strings.ToUpper(customer)
-	return "Welcome to the Tech Palace, "+customerName
+    customerName := strings.ToUpper(customer)
+    return "Welcome to the Tech Palace, "+customerName
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	starsInLine := strings.Repeat("*", numStarsPerLine)
+    starsInLine := strings.Repeat("*", numStarsPerLine)
     return starsInLine +"\n"+welcomeMsg+"\n"+starsInLine
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	removedBorder := strings.ReplaceAll(oldMsg, "*", "")
+    removedBorder := strings.ReplaceAll(oldMsg, "*", "")
     newMsg := strings.Trim(removedBorder, "\n ") 
 
     return newMsg
